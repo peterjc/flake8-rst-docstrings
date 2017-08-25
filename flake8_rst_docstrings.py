2d9085bd7189c0411ce0614d853b314e86ae08bb
Peter Cock <p.j.a.cock@googlemail.com>
2017-08-25 15:25:56 +0100

    flake8 thyself. Copy-and-paste spaces, grr

--- flake8_rst_docstrings.py
+++ flake8_rst_docstrings.py
@@ -254,11 +254,11 @@ def dequote_docstring(text):
         return text[4:-3]
     if len(text) > 2 and text[0] == text[-1] == '"':
         return text[1:-1]
-    if len(text) > 3 and text[:2] in ('u"', 'r"') and  text[-1] == '"':
+    if len(text) > 3 and text[:2] in ('u"', 'r"') and text[-1] == '"':
         return text[2:-1]
     if len(text) > 2 and text[0] == text[-1] == "'":
         return text[1:-1]
-    if len(text) > 3 and text[:2] in ("u'", "r'") and  text[-1] == "'":
+    if len(text) > 3 and text[:2] in ("u'", "r'") and text[-1] == "'":
         return text[2:-1]
     raise ValueError("Bad quotes!")
 

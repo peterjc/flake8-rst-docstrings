ac4c7a9bf27bfc0062c1107aff23a3a82821c933
Peter Cock <p.j.a.cock@googlemail.com>
2017-08-25 15:23:42 +0100

    Cope with unicode and raw markers

--- flake8_rst_docstrings.py
+++ flake8_rst_docstrings.py
@@ -239,16 +239,27 @@ def trim(docstring):
 
 def dequote_docstring(text):
     """Remove the quotes delimiting a docstring."""
+    # TODO: Process escaped characters unless raw mode?
     text = text.strip()
     if len(text) > 6 and text[:3] == text[-3:] == '"""':
+        # Standard case, """..."""
         return text[3:-3]
+    if len(text) > 7 and text[:4] in ('u"""', 'r"""') and text[-3:] == '"""':
+        # Unicode, u"""...""", or raw r"""..."""
+        return text[4:-3]
     # Other flake8 tools will report atypical quotes:
     if len(text) > 6 and text[:3] == text[-3:] == "'''":
         return text[3:-3]
+    if len(text) > 7 and text[:4] in ("u'''", "r'''") and text[-3:] == "'''":
+        return text[4:-3]
     if len(text) > 2 and text[0] == text[-1] == '"':
         return text[1:-1]
+    if len(text) > 3 and text[:2] in ('u"', 'r"') and  text[-1] == '"':
+        return text[2:-1]
     if len(text) > 2 and text[0] == text[-1] == "'":
         return text[1:-1]
+    if len(text) > 3 and text[:2] in ("u'", "r'") and  text[-1] == "'":
+        return text[2:-1]
     raise ValueError("Bad quotes!")
 
 

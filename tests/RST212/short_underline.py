b3df111d33f413ed08c5ee6e6826742484871c64
peterjc <p.j.a.cock@googlemail.com>
2019-04-23 15:22:57 +0100

    Test for RST212 Title underline too short.

---
+++ tests/RST212/short_underline.py
@@ -0,0 +1,22 @@
+"""Print 'Hello world' to the terminal.
+
+Long
+=====
+An overly long underline (here one extra equals sign)
+is considered acceptable.
+
+Short
+====
+There is a missing equals sign on the above underline,
+and that is considered an error. This should fail::
+
+    $ flake8 --select RST RST212/short_underline.py
+    RST212/short_underline.py:10:1: RST299 Title underline too short.
+
+Nice
+====
+Finally, this underline is just right.
+
+"""
+
+print("Hello world")

f8e6a8350c049d4f7168c854ba945403dd5fa794
Peter Cock <p.j.a.cock@googlemail.com>
2019-11-18 11:55:54 +0000

    Test for RST210

---
+++ tests/RST210/strong.py
@@ -0,0 +1,15 @@
+"""Print 'Hello world' to the terminal.
+
+RST uses double asterisks for **strong**, which is
+normally rendered as **bold**.
+
+Here **strong is missing a closing double asterisk.
+
+That is considered to be an error, and should fail::
+
+    $ flake8 --select RST RST210/strong.py
+    RST210/strong.py:7:1: RST210 Inline strong start-string without end-string.
+
+"""
+
+print("Hello world")

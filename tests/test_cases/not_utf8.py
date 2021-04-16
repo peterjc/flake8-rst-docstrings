24b48d9ca5905da68581c490c37da37ee607222c
Peter Cock <p.j.a.cock@googlemail.com>
2021-04-16 09:14:38 +0100

    Example not using UTF-8 but a different encoding.
    If loaded as UTF-8, expect a UnicodeDecodeError.

---
+++ tests/test_cases/not_utf8.py
@@ -0,0 +1,30 @@
+# -*- coding: iso-8859-1 -*-
+"""This file is not using UTF-8, but Western European.
+
+It works fine using the Windows-1252 or iso-8859-1 encoding,
+but not using UTF-8 which is the Python default.
+
+RST docstring validation is expected to fail:
+
+
+    $ flake8 --select RST RST903/not_utf8.py
+    XXX
+
+"""
+
+
+class Name:
+    """Trivial class for a person's name."""
+
+    def __init__(self, first, last):
+        """Initialize name instance.
+
+        >>> fred = Name("Frédéric", "François")
+        >>> print(fred)
+        """
+        self.first = first
+        self.last = last
+
+    def __str__(self):
+        """Return European style name, First Last."""
+        return "%s %s" % (self.first, self.last)

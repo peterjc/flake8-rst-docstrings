bffc36969ec16f879752d40800f6a29eb9da2f0c
Peter Cock <p.j.a.cock@googlemail.com>
2021-04-16 12:24:19 +0100

    Test case for RST205 Explicit markup ends without a blank line

---
+++ tests/RST205/explicit_markup.py
@@ -0,0 +1,26 @@
+"""Example of invalid RST explicit markup.
+
+This should fail validation:
+
+    $ flake8 --select RST RST205/explicit_markup.py
+    RST205/explicit_markup.py:21:1: RST205 Explicit markup ends without a blank line; unexpected unindent.
+
+See below.
+"""  # noqa: E501
+
+
+def function(args):
+    """Do something.
+
+    This is a malformed explicit markup block in RST:
+
+    ..
+      Alpha
+      Beta
+      Gamma
+    and so on to Omega.
+
+    That line about Omega should be indented for preceeded
+    by a blank line.
+    """
+    pass

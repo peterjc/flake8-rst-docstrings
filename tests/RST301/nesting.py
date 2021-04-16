c333f7fc4ffcc196a55053efbfd294eaa930e983
Peter Cock <p.j.a.cock@googlemail.com>
2021-04-16 11:59:24 +0100

    Test case for RST301 Unexpected indentation.

---
+++ tests/RST301/nesting.py
@@ -0,0 +1,28 @@
+"""Example of invalid nested bullets in RST.
+
+This file should fail RST validation:
+
+    $ flake8 --select RST RST301/nesting.py
+    RST301/nesting.py:21:1: RST301 Unexpected indentation
+
+See below.
+"""
+
+
+def function(args):
+    """Do something.
+
+    This looks like a nested bullet point list,
+    doesn't it?
+
+    - aaaa
+      - bb1
+      - bb2
+        - ccc
+    - aaaa
+    - aaaa
+
+    However, RST would require blank lines between the
+    different levels of nesting.
+    """
+    pass

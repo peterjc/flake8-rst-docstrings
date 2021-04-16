796eead15b23bf1a04c644c8ec1078deb097a871
Peter Cock <p.j.a.cock@googlemail.com>
2021-04-16 12:09:27 +0100

    Test case for RST401 Unexpected section title.

---
+++ tests/RST401/sub_title.py
@@ -0,0 +1,26 @@
+"""Example breaking RST401.
+
+This should fail validation:
+
+    $ flake8 --select RST RST401/sub_title.py
+    RST401/sub_title.py:20:1: RST401 Unexpected section title
+
+See below.
+"""
+
+
+def function(args):
+    """Do something.
+
+    Paragraph here, then an indented chunk...
+
+        Indented text.
+
+        Unexpected Subtitle
+        -------------------
+
+        More indented text.
+
+    Back to the main level.
+    """
+    pass

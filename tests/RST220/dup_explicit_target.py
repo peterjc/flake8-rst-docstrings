60bb674a06e24080fdb4402466e2262486a1fc62
Peter J. A. Cock <p.j.a.cock@googlemail.com>
2025-10-06 18:06:44 +0100

    Old example downgraded to INFO in docutils 0.22

--- tests/RST220/dup_explicit_target.py
+++ tests/RST220/dup_explicit_target.py
@@ -1,25 +1,19 @@
 """Print 'Hello world' to the terminal; test links in docstrings.
 
-It is common in RST to use unnamed or anonymous links in text (like
-`here <https://example.com/t-and-c/>`__ and `here
-<https://example.org/co-op-rules.html>`__), but they should have two trailing
-underscores!
-
-You *can* use a `single trailing underscore <https://example.com/links.html>`_
-but it is treated as an implicitly named link. You can use the exact same link
-text and URL again too (`single trailing underscore
-<https://example.com/links.html>`_).
-
-However, the missing second underscore becomes a problem if the same link text
-is used again for another implicitly named link with a *different* URL (e.g.
-missing `single trailing underscore <https://example.com/bad.html>`_).
+Suppose we want to link to the Python_ home page, but have duplicate
+link targets defined?
 
 What happens here?
 
     $ flake8 --select RST RST220/dup_explicit_target.py
-    RST220/dup_explicit_target.py:15:1: RST220 Duplicate explicit target name: "single trailing underscore".
+    RST220/dup_explicit_target.py:3:1: RST399 Duplicate target name, cannot be used as a unique reference: "python".
+    RST220/dup_explicit_target.py:15:1: RST220 Duplicate explicit target name: "python".
 
-This file triggers an error when the implicitly named link is redefined.
+This file triggers an error as there are repeated conflicting definitions:
+
+.. _Python: https://www.python.org
+.. _Python: https://python.org
+
 """  # noqa: E501
 
 print("Hello world")
